2ac659d8d8427f1eb5e0f601d00c733ed73f58aa
Maxine Whitely <m.whitely16@gmail.com>
2017-07-21 10:43:17 -0500

    prints x # of selected assets with file info

--- videoMaker.js
+++ videoMaker.js
@@ -12,6 +12,7 @@ function openFinder() {
 function GetFileSizeNameAndType() {
 	var fi = document.getElementById('file'); // GET THE FILE INPUT AS VARIABLE.
 
+	var totalFileSize = 0;
 	// VALIDATE OR CHECK IF ANY FILE IS SELECTED.
 	if (fi.files.length > 0)
 	{
@@ -19,14 +20,18 @@ function GetFileSizeNameAndType() {
 		for (var i = 0; i <= fi.files.length - 1; i++)
 		{
 			//ACCESS THE SIZE PROPERTY OF THE ITEM OBJECT IN FILES COLLECTION. IN THIS WAY ALSO GET OTHER PROPERTIES LIKE FILENAME AND FILETYPE
+			var fsize = fi.files.item(i).size;
+			totalFileSize = totalFileSize + fsize;
 			document.getElementById('fp').innerHTML =
 			document.getElementById('fp').innerHTML
-			+ '</b> Name: <b>' fi.files.item(i).name
+			+ '<br /> ' + 'File: <b>' + fi.files.item(i).name
 			+ '</b> Size: <b>' + Math.round((fsize / 1024)) //DEFAULT SIZE IS IN BYTES SO WE DIVIDING BY 1024 TO CONVERT IT IN KB
-			+ '</b> KB. Type: <b>' + fi.files.item(i).type + "</b>.";
+			+ '</b> Type: <b>' + fi.files.item(i).type + "</b>.";
 		}
 	}
+	document.getElementById('divTotalSize').innerHTML = "Total File(s) Size is <b>" + Math.round(totalFileSize / 1024) + "</b> KB";
 }
+
 
 
 function makeVideo() {
