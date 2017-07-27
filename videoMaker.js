33a9ad1a798d38547fbd3a96ee0d08fa9d55d802
jbirori <jeffbirori@gmail.com>
2017-07-27 10:40:05 -0500

    Replaced the constant with a variable so the user can add as many videos as they want and have them all merged.

--- videoMaker.js
+++ videoMaker.js
@@ -56,6 +56,9 @@ function move() {
 }
 
 function makeVideo() {
+	var fi = document.getElementById('file');
+	var videoCount = fi.files.length;
+
 	document.getElementById('processing').innerHTML = "We're making your video! Give us a few.";
 	move();
 
@@ -90,7 +93,7 @@ function makeVideo() {
 				console.log(ii, jj);
 				mergedVideo = mergedVideo.addInput('output' + jj + '.mov');
 				jj++;
-				if (jj == 2) { // replace 2 with the number of videos that the user inputs
+				if (jj == videoCount) { // replace 2 with the number of videos that the user inputs
 					mergedVideo.mergeToFile('./mergedVideo.mov', './tmp/')
 					.videoCodec('libx264')
 		    	.audioCodec('libmp3lame')
