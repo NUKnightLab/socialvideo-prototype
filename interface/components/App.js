e7874bcc57bda84c8d67c445c053380bdea7edf2
Maxine Whitely <m.whitely16@gmail.com>
2017-08-01 15:12:38 -0500

    added React app setup for interface build

---
+++ interface/components/App.js
@@ -0,0 +1,21 @@
+var React = require('react');
+var Popular = require('./Popular')
+
+/*
+a compononent is concerned about three things that can live within it: 
+states, lifecycle events, and UI. The way to set UI is with the render 
+method: whatever render returns is the UI of that component.
+*/
+
+//Component defintion
+class App extends React.Component {
+	render() {
+		return (
+			<div className='container'>
+				<Component />
+			</div>
+		)
+	}
+}
+
+module.exports = App;
