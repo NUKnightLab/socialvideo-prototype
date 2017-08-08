7ca1fd9643cbbe04f049132e13c1ab4ae7e3b219
Maxine Whitely <m.whitely16@gmail.com>
2017-08-08 12:02:09 -0500

    added Chunk component with styling to model our UI mockup + a chunk counter/chunk add button

---
+++ interface/components/ChunkCounter.jsx
@@ -0,0 +1,27 @@
+ import React from 'react';
+ import '../www/index.css';
+
+ /**
+ * A counter button: tap the button to increase the count.
+ */
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+ 
+  render() {
+    return (
+      <button
+        onClick={() => {
+          this.setState({ count: this.state.count + 1 });
+        }}
+      >
+        Count: {this.state.count}
+      </button>
+    );
+  }
+}
+export default Counter;
