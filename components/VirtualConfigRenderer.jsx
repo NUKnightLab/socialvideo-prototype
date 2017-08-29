02cafb2a50f45a94a47d594feb08642b8702d713
Maxine Whitely <m.whitely16@gmail.com>
2017-08-29 12:16:53 -0500

    static global music, static logo, dynamic filename/filetype

--- components/VirtualConfigRenderer.jsx
+++ components/VirtualConfigRenderer.jsx
@@ -6,6 +6,7 @@ import TextChunker from './TextChunker.jsx';
 import EditorWindow from './EditorWindow.jsx';
 import GlobalPresetBar from './GlobalPresetBar.jsx';
 import MediaLibrary from './MediaLibrary.jsx';
+import InitMenu from './InitMenu.jsx';
 
 class VirtualConfigRenderer extends React.Component {
   constructor(props) {
@@ -13,12 +14,14 @@ class VirtualConfigRenderer extends React.Component {
     this.state = {
       videoObjects: [],
       globalPresets: {color: '#ffffff', font: 'Verdana.ttf',},
-      mediaFiles: []
+      mediaFiles: [],
+      fileName: 'video.mov'
     }
     this.createVideoObjects = this.createVideoObjects.bind(this);
     this.updateVideoObjects = this.updateVideoObjects.bind(this);
     this.trackMediaFiles = this.trackMediaFiles.bind(this);
     this.updateGlobal = this.updateGlobal.bind(this);
+    this.nameFile = this.nameFile.bind(this);
     this.makeVideo = this.makeVideo.bind(this);
   }
 
@@ -38,31 +41,40 @@ class VirtualConfigRenderer extends React.Component {
     this.setState({globalPresets: updatedGlobals});
   }
 
+  nameFile(inputtedName) {
+    this.setState({ fileName: inputtedName });
+  }
+
   makeVideo() {
-    makeVideo(this.state.videoObjects, this.state.globalPresets);
+    makeVideo(this.state.videoObjects, this.state.globalPresets, this.state.fileName)
+      //.then(addAudio(this.state.fileName), console.log('lol nope1'))
+      //.then(addLogo(this.state.fileName), console.log('lol nope2'))
   }
 
   render() {
     return (
       <div>
-        <Flexbox flexDirection="row">
-          <div className="media-lib-container">
-            <MediaLibrary 
-              trackMediaFiles={this.trackMediaFiles}
-              mediaFiles={this.state.mediaFiles} />
+        <div className="init-menu-container">
+          <InitMenu 
+            fileName={this.state.fileName}
+            nameFile={this.nameFile} />
+        </div>
+        <div className="media-lib-container">
+          <MediaLibrary 
+            mediaFiles={this.state.mediaFiles}
+            trackMediaFiles={this.trackMediaFiles} />
+        </div>
+        <div className="editor-container">
+          <GlobalPresetBar 
+            globalPresets={this.state.globalPresets}
+            updateGlobal={this.updateGlobal} />
+          <TextChunker 
+            createVideoObjects={this.createVideoObjects} />
+          <EditorWindow
+            videoObjects={this.state.videoObjects}
+            updateVideoObjects={this.updateVideoObjects} />
+            <button onClick={this.makeVideo}> Make Video! </button>
           </div>
-          <div className="editor-container">
-            <GlobalPresetBar 
-              globalPresets={this.state.globalPresets}
-              updateGlobal={this.updateGlobal} />
-            <TextChunker 
-              createVideoObjects={this.createVideoObjects} />
-            <EditorWindow
-                videoObjects={this.state.videoObjects}
-                updateVideoObjects={this.updateVideoObjects} />
-              <button onClick={this.makeVideo}> Make Video! </button>
-            </div>
-          </Flexbox>
       </div>
     );
   }
--- www/videoMaker.js
+++ www/videoMaker.js
@@ -5,7 +5,7 @@ var tmp = require('tmp'); //Allows creation of temporary files + directories
 var tmpobj = tmp.dirSync({unsafeCleanup: true}); // Synchronous directory creation
 
 
-function makeVideo(videoObjects, globalPresets) {
+function makeVideo(videoObjects, globalPresets, fileName) {
 	var videoCount = videoObjects.length;
 	var ii = 0;
 	var jj = 0;
@@ -38,21 +38,22 @@ function makeVideo(videoObjects, globalPresets) {
 					}
 			})
 			.videoCodec('libx264')
-    		.audioCodec('libmp3lame')
+    		.noAudio()
+    		//.audioCodec('libmp3lame')
 			.size('800x660')
 			.format('mov')
 			.outputOptions('-movflags frag_keyframe+empty_moov')
 		  	.on('error', function(err) {
-            console.log('An error occurred: ' + err.message);
+            	console.log('An error occurred: ' + err.message);
 		  	})
 		  	.on('end', function() {
-		    console.log('Processing finished !');
+		    	console.log('Processing finished !');
 
 				console.log(ii, jj);
 				mergedVideo = mergedVideo.addInput(tmpobj.name + '/' + jj + '.mov');
 				jj++;
-				if (jj == videoCount) { // replace 2 with the number of videos that the user inputs
-					mergedVideo.mergeToFile('./mergedVideo.mov', './tmp/')
+				if (jj == videoCount) {
+					mergedVideo.mergeToFile('0'+fileName, './tmp/')
 					.videoCodec('libx264')
 		    		.audioCodec('libmp3lame')
 					.format('mov')
@@ -62,13 +63,38 @@ function makeVideo(videoObjects, globalPresets) {
 					})
 					.on('end', function() {
 					    document.getElementById('processing').innerHTML = "Finished!";
+					    addAudio(fileName);
 					    tmpobj.removeCallback(); //trashes temporary directory
 					});
 				}
 			})
 			.pipe(outStream, { end: true });
-
 		ii++;
-	});
+	})
+}
 
+function addAudio(fileName) {
+	fluent_ffmpeg()
+		.input('0'+fileName)
+		.input('./music.mp3')
+		.outputOptions([
+			'-codec copy',
+			'-shortest'
+			])
+		.save('1'+fileName)
+   		.on('end', function() {
+   			addLogo(fileName);
+   			console.log('adding logo!');
+   		})
+}
+
+function addLogo(fileName) {
+	fluent_ffmpeg()
+		.input('1'+fileName)
+		.input('logo.png')
+		.complexFilter('[0:v][1:v] overlay=25:25')
+		.save(fileName)
+		.on('progress', function(progress) {
+   		  	console.log('Processing: ' + progress.percent + '% done');
+   		})
 }
