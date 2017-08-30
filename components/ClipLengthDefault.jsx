b4789e8dd070bb48a33ab8e3101cccadd845487d
Maxine Whitely <m.whitely16@gmail.com>
2017-08-30 14:56:24 -0500

    text timing updates with new input and with new text input

--- components/ClipLengthDefault.jsx
+++ components/ClipLengthDefault.jsx
@@ -4,22 +4,25 @@ import '../www/index.css';
 class ClipLengthDefault extends React.Component {
 	constructor(props) {
 		super(props);
-		//this.setTextTiming = this.setTextTiming.bind(this);
+		this.setTextTiming = this.setTextTiming.bind(this);
 	}
 
- 	// setTextTiming(inputTime) {
- 	// 	var videoObjects = this.props.videoObjects;
-		// var timing = this.props.timing;
-		// videoObjects.text_timing = inputTime;
-		// this.props.updateVideoObjects[videoObjects];
- 	// }
+ 	setTextTiming() {
+ 		var videoObjects = this.props.videoObjects;
+		var timing = this.props.timing;
+		var position = this.props.position;
+		var id = String(position) + "text-input";
+		videoObjects[position].text_timing = document.getElementById(id).value;
+		this.props.updateVideoObjects(videoObjects);
+ 	}
 
 	render() {
 		return (
 			<div className="Chunk-video-start">
 				<p className="Chunk-video-start-seconds"> Length: </p>
 				<textarea 
-					className="Chunk-video-start-input" 
+					className="Chunk-video-start-input"
+					id={String(this.props.position) + "text-input"} 
 					defaultValue={this.props.timing}
 					onChange={this.setTextTiming}>
 				</textarea>
